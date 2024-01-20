bd5f78cc28bcf81cdfae35318c82e3f5f57afac2
arvin <arvinsaguisa@gmail.com>
2024-01-20 23:16:09 +0800

    Warmup credisync

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import credisynclogo from '../Images/credisynclogo.png';
+import './LoginPage.css'
+
+const LoginPage = () => {
+  return (
+    <div className='wrapper'>
+     <form action="">
+        <img src={credisynclogo} alt="logo" height={210} width={210} />   
+        <h1>Sign In</h1>
+
+        <label>Email</label>
+        <div className='email-input'>
+            <input type="text" placeholder='Enter your email' required/>
+        </div>
+
+        <label>Password</label>
+        <div className='password-input'>
+            <input type="password" placeholder='Enter your password' required/>
+        </div>
+
+        <div className='forgot-link'>
+            <a href='#'>Forgot your password?</a>
+        </div>
+        
+        <button type="submit">Sign In</button>
+
+        <div className='signup-link'>
+            <a href="#">Don't have an account? Sign up</a>
+        </div>
+     </form>
+    </div>
+  )
+}
+
+export default LoginPage
